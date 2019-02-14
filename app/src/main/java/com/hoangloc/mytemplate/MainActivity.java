e8ccfa1ac00dec5173a06d2d5560dbac294a5bb6
Hoang Loc <1511849@hcmut.edu.vn>
2019-02-14 18:22:21 +0700

    Add Tab Activity

--- app/src/main/java/com/hoangloc/mytemplate/MainActivity.java
+++ app/src/main/java/com/hoangloc/mytemplate/MainActivity.java
@@ -1,13 +1,84 @@
 package com.hoangloc.mytemplate;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_layout);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        FragmentTransaction transaction;
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, new FragmentMain());
+        transaction.commit();
+
+        NavigationView navigationView =  findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.nav_tab:
+                                Toast.makeText(getBaseContext(),"Tab Activity", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), ActivityTab.class);
+                                startActivity(intent);
+                                break;
+                            case R.id.nav_gallery:
+                                Toast.makeText(getBaseContext(),"Gallery", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_manage:
+                                Toast.makeText(getBaseContext(),"Manage", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_slideshow:
+                                Toast.makeText(getBaseContext(),"Slide Show", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+
+                        }
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+//                Toast.makeText(getBaseContext(),"Ahihi", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }

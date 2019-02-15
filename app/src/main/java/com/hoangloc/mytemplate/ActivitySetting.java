0e52f9360d25d33ef4779c6536b5d8bbeb2e81cd
Hoang Loc <1511849@hcmut.edu.vn>
2019-02-15 15:40:42 +0700

    Handle Preference Events

--- app/src/main/java/com/hoangloc/mytemplate/ActivitySetting.java
+++ app/src/main/java/com/hoangloc/mytemplate/ActivitySetting.java
@@ -1,14 +1,58 @@
 package com.hoangloc.mytemplate;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class ActivitySetting extends PreferenceActivity {
+    CheckBoxPreference checkBoxPreference;
+    SwitchPreference switchPreference;
+    EditTextPreference editTextPreference;
+    ListPreference listPreference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.activity_setting);
+        checkBoxPreference = (CheckBoxPreference) findPreference("checkbox");
+        checkBoxPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (checkBoxPreference.isChecked())
+                    Toast.makeText(getBaseContext(),"Checkkkkkkk", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getBaseContext(),"Uncheckkkkkkk", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        switchPreference = (SwitchPreference) findPreference("switchbox");
+        switchPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (switchPreference.isChecked())
+
+                    Toast.makeText(getBaseContext(),"Switch On", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getBaseContext(),"Switch Off", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+
+        editTextPreference = (EditTextPreference) findPreference("editbox");
+//        editTextPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+//            @Override
+//            public boolean onPreferenceChange(Preference preference, Object newValue) {
+//
+//                return false;
+//            }
+//        });
     }
 }
